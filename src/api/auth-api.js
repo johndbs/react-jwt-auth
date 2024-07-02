885357acefdded786a5585c9d164ac832aa4bd72
johndbs <83272366+johndbs@users.noreply.github.com>
2024-07-02 12:04:21 +0200

    fix: the call of the post /auth/signup with parameters

--- src/api/auth-api.js
+++ src/api/auth-api.js
@@ -3,7 +3,11 @@ import { API_URL } from "../constants";
 
 export class AuthApi {
     static async register(username, email, password) {
-        return axios.post(`${API_URL}/auth/signup`, { username, email, password });
+        return axios.post(`${API_URL}/auth/signup`, { 
+            username: username, 
+            email: email, 
+            password: password
+        });
     }
 
     static async login(username, password) {
